ca956eae30d982232a1d9e86a135babf76c99792
Luke Karrys <lukekarrys@gmail.com>
2018-05-11 16:14:41 -0700

    no ga

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-/* globals ga */
-
 import './styles/theme.less';
 import '!!file-loader?name=favicon.png!./favicon.png';
 import '!!file-loader?name=favicon.ico!./favicon.ico';
@@ -16,16 +14,7 @@ import {auth} from 'l/firebase';
 import Routes from './routes';
 import configureStore from './store/configureStore';
 
-// eslint-disable-next-line no-console
-const analytics = (process.env.NODE_ENV === 'production' ? ga : console.log);
 const store = configureStore();
-
-// Google analytics for each history change
-const pageview = ({pathname, search}) => analytics('send', 'pageview', pathname + search);
-history.listen(pageview);
-pageview(history.location);
-
-window.__history = history;
 
 store.dispatch(meActions.loginStart());
 auth.onAuthStateChanged(once((user) => store.dispatch(meActions.authUser(user))));
